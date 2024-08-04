60c15bef30299951e33e62017eabec9d9a89cc8d
g0r0kh <gorokhovivan09@gmail.com>
2024-08-04 22:15:27 +0300

    add external file name column

--- 5 mult pdf imp.py
+++ 5 mult pdf imp.py
@@ -8,6 +8,8 @@ import tabula # call pdf viewer
 pdf_files = glob.glob('*.pdf') # directory viewer for exac files
 
 df = pd.DataFrame()
+filename_col = pd.Series([],
+                         dtype='str') # add file name column
 for pdf_file in pdf_files:
     pdf_table = tabula.read_pdf(pdf_file,
                              pages='all',
@@ -17,8 +19,18 @@ for pdf_file in pdf_files:
     for pdf_table in pdf_tables:
         if pdf_table.columns[0] == 'Цифр. код':
            df = pd.concat([df, pdf_table])
+
+           filename = pd.Series([pdf_file]).repeat(len(pdf_table.index)) # add file name column
+           filename_col = filename_col._append(filename) # add file name column
         else:
            continue
+# necessary exec for series concat
+
+df = df.reset_index(drop=True)
+filename_col = filename_col.reset_index(drop=True)
+df = pd.concat([df, filename_col.rename('file_name')],
+               axis=1)
+
 
 
 # df_single.to_csv('pdf_2_csv.csv',
